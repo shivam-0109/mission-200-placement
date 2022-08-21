99ebf1436c0956d8d469d84db34c75eefd693da6
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-08-22 00:21:49 +0530

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //DATA TYPE AND ITS SIZE
+    
+    cout<<"SIZE of int "<<sizeof(int)<<endl;
+    cout<<"SIZE of char "<<sizeof(char)<<endl;
+    cout<<"SIZE of float "<<sizeof(float)<<endl;
+    cout<<"SIZE of bool "<<sizeof(bool)<<endl;
+
+
+    
+    return 0;
+}
