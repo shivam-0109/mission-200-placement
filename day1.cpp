4e94d3ed90541078ea0975ab5b5980217a6a1f2d
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-08-22 00:23:48 +0530

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -4,10 +4,18 @@ using namespace std;
 int main(){
     //DATA TYPE AND ITS SIZE
     
-    cout<<"SIZE of int "<<sizeof(int)<<endl;
-    cout<<"SIZE of char "<<sizeof(char)<<endl;
-    cout<<"SIZE of float "<<sizeof(float)<<endl;
-    cout<<"SIZE of bool "<<sizeof(bool)<<endl;
+    // cout<<"SIZE of int "<<sizeof(int)<<endl;
+    // cout<<"SIZE of char "<<sizeof(char)<<endl;
+    // cout<<"SIZE of float "<<sizeof(float)<<endl;
+    // cout<<"SIZE of bool "<<sizeof(bool)<<endl;
+    //basic input output
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a/b<<endl;
+    cout<<a%b<<endl;
 
 
     
