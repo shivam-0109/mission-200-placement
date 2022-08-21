c3df11b499f79b4a6188e2dee75ef79b5786df3b
shivam kumar singh <89032612+shivam-0109@users.noreply.github.com>
2022-08-22 00:28:55 +0530

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -9,13 +9,23 @@ int main(){
     // cout<<"SIZE of float "<<sizeof(float)<<endl;
     // cout<<"SIZE of bool "<<sizeof(bool)<<endl;
     //basic input output
-    int a,b;
-    cin>>a>>b;
-    cout<<a+b<<endl;
-    cout<<a-b<<endl;
-    cout<<a*b<<endl;
-    cout<<a/b<<endl;
-    cout<<a%b<<endl;
+    // int a,b;
+    // cin>>a>>b;
+    // cout<<a+b<<endl;
+    // cout<<a-b<<endl;
+    // cout<<a*b<<endl;
+    // cout<<a/b<<endl;
+    // cout<<a%b<<endl;
+    //if else
+    int saving;
+    cin>>saving;
+    if(saving>=5000){
+        cout<<"GO with 1st one"<<endl;
+    }else if(saving>=3000){
+        cout<<"GO with 2nd ones"<<endl;
+    }else{
+        cout<<"Go with freind"<<endl;
+    }
 
 
     
